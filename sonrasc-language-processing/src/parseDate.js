4a3dbe7fb8ef0a92dd52c12e8b218e9dd6ddb6a2
John Brennan <john.brennan55@mail.dcu.ie>
2016-04-25 15:16:45 +0100

    more date parsing.

--- sonrasc-language-processing/src/parseDate.js
+++ sonrasc-language-processing/src/parseDate.js
@@ -1,8 +1,15 @@
+import moment from 'moment';
+
 const parseDate = (date) => {
+  date = date.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\]/gi, '');
   let data = date.split('/');
   console.log(data);
-  console.log(new Date(data[2], data[1], data[0]));
-  return new Date(data[2], data[1]-1, data[0]);
+  if (new Date(data[2], data[1], data[0]).toString() !== 'Invalid Date') return new Date(data[2], data[1]-1, data[0]);
+  //else return moment(date)
+  else {
+    console.log(moment(date));
+    return moment(date);
+  }
 };
 
 export default parseDate;
